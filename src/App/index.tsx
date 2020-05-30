b512ff005eeec25eaf4deeed2e675fc155c61d4a
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-05-30 23:33:18 +0600

    Add exact routing to routes

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Header from '../component/Header';
+import Slider from '../component/Slider';
 
 const App: React.FC = () => {
-  const Home = <p> Home </p>;
+  const Home = <Slider />;
   const About = <p> About </p>;
   return (
     <>
       <Header />
       <Switch>
-        <Route path="/about">{About}</Route>
-        <Route path="/">{Home}</Route>
+        <Route path="/about" exact={true}>
+          {About}
+        </Route>
+        <Route path="/" exact={true}>
+          {Home}
+        </Route>
       </Switch>
     </>
   );
