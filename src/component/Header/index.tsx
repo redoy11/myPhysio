ff81f239e351f8831288ac861db71b0241125768
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-05-30 23:32:45 +0600

    Add two sets of toolbar to header

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -6,6 +6,11 @@ const Header: React.FC = () => {
   return (
     <React.Fragment>
       <AppBar position="fixed">
+        <Toolbar style={{ justifyContent: 'space-between' }}>
+          <Typography variant="h4">National Health</Typography>
+          <Typography variant="h6">Call 0199999 to book appointment</Typography>
+          <Typography variant="h6">Call 0199999 to book appointment</Typography>
+        </Toolbar>
         <Toolbar>
           <Typography variant="h6">Home</Typography>
           <Typography variant="h6">About</Typography>
