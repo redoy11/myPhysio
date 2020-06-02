f5c19db460017b5a92b7ed66237d86e417f55015
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-06-02 22:15:20 +0600

    Add Services constant

---
+++ src/component/Services/constants.ts
@@ -0,0 +1,20 @@
+export const SERVICES = [
+  {
+    iconName: 'import_contacts',
+    title: 'Information',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    iconName: 'account_box',
+    title: 'Advice',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    iconName: 'support',
+    title: 'Support',
+    body:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
