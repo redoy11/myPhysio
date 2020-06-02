481521e7a66782278a57df1d9c1b6cc9ed9a79f2
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-06-02 23:16:07 +0600

    Add a custom palette to theme

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,21 +3,34 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Header from '../component/Header';
 import Home from '../component/Home';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 
 const App: React.FC = () => {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: '#7FCD46',
+        main: '#4A9F04',
+        dark: '#0F4C00',
+        contrastText: 'white',
+      },
+    },
+  });
   const About = <p> About </p>;
 
   return (
     <>
-      <Header />
-      <Switch>
-        <Route path="/about" exact={true}>
-          {About}
-        </Route>
-        <Route path="/" exact={true}>
-          <Home />
-        </Route>
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <Header />
+        <Switch>
+          <Route path="/about" exact={true}>
+            {About}
+          </Route>
+          <Route path="/" exact={true}>
+            <Home />
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </>
   );
 };
