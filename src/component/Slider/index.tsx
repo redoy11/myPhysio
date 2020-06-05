471b6421ff74ff43419fc06b0e4859d5bed57168
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-06-06 00:09:15 +0600

    Refactor Slide to use css cross fading technique

--- src/component/Slider/index.tsx
+++ src/component/Slider/index.tsx
@@ -1,30 +1,24 @@
 import * as React from 'react';
-import makeCarousel from 'react-reveal/makeCarousel';
-import Slide from 'react-reveal/Slide';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const CarouselUI = ({ children }: any) => <div style={{ width: '100%', height: '100vh' }}>{children}</div>;
-
-const Carousel = makeCarousel(CarouselUI);
+import './Slider.css';
 
 const Slider: React.FC = () => {
   return (
-    <Carousel defaultWait={3000}>
-      <Slide right={true}>
+    <div className="Slider-container">
+      <div className="Slider-slide top">
         <img src="/myPhysio/img_remedy.jpg" style={{ width: '100%', height: '100%' }} />
         <div style={{ position: 'absolute', top: '50%', left: '50%' }}>
           <h1>Slide 1</h1>
           <p>Slide Description</p>
         </div>
-      </Slide>
-      <Slide right={true}>
+      </div>
+      <div className="Slider-slide bottom">
         <img src="/myPhysio/img_foot.jpg" style={{ width: '100%', height: '100%' }} />
         <div style={{ position: 'absolute', top: '50%', left: '50%' }}>
           <h1>Slide 2</h1>
           <p>Slide Description</p>
         </div>
-      </Slide>
-    </Carousel>
+      </div>
+    </div>
   );
 };
 
