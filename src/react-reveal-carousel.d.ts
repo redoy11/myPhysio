c269120b3e2bd56c880fb37a42b3fa296feff674
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-06-06 00:15:56 +0600

    Remove the redundant type definitions of react-reveal library

--- src/react-reveal-carousel.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module 'react-reveal/makeCarousel' {
-  declare const makeCarousel: any;
-  export default makeCarousel;
-}
--- src/react-reveal-slide.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module 'react-reveal/Slide' {
-  declare const Slide: any;
-  export default Slide;
-}
