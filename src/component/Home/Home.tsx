cbd1f9e0a0243286f57ba6b9833147dbc329f4e3
Nafiz Ahmed <redoyahmed11@gmail.com>
2020-07-19 01:37:32 +0600

    Fix Layout and add home background.

--- src/component/Home/Home.tsx
+++ src/component/Home/Home.tsx
@@ -3,17 +3,18 @@ import './Home.scss';
 import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 import Header from './Header/Header';
 
-const HOME_SECTION_COMPONENTS = ['home', 'about', 'services', 'review', 'news', 'contact'];
+const HOME_SECTIONS = ['home', 'about', 'services', 'review', 'news', 'contact'];
+const HOME_SECTION_COMPONENTS: React.Component[] = [];
 
 const Home: React.FC = () => {
   return (
     <React.Fragment>
       <Header />
       <MDBContainer className="Home-container" fluid>
-        {HOME_SECTION_COMPONENTS.map((sectComp, index) => (
+        {HOME_SECTIONS.map((sectComp, index) => (
           <MDBRow key={`home-section-` + index}>
             <MDBCol size="12">
-              <section id={'section-' + sectComp}>{sectComp}</section>
+              <section id={'section-' + sectComp}>{HOME_SECTION_COMPONENTS[index] || null}</section>
             </MDBCol>
           </MDBRow>
         ))}
